55c879ea854d5b260b7152ba497863bf8dc41133
9866494 <9866494@gmail.com>
2015-08-18 10:35:10 +0300

    Scaffold with yeoman angular-fullstack generator

---
+++ client/components/navbar/navbar.directive.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('meanCatalogApp')
+  .directive('navbar', function () {
+    return {
+      templateUrl: 'components/navbar/navbar.html',
+      restrict: 'E',
+      controller: 'NavbarCtrl'
+    };
+  });
