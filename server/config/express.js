49117a8b490511f0da5d6c5fe86587c6bde281b3
awful <9866494@gmail.com>
2016-08-30 22:43:19 +0300

    Changed the path to static to path.join

--- server/config/express.js
+++ server/config/express.js
@@ -14,28 +14,28 @@ var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
 var path = require('path');
 var config = require('./environment');
-var multer  = require('multer')
+var multer = require('multer');
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, __dirname+'/../../client/file_uploads/');
+    cb(null, path.join(__dirname, '..', '..', 'client', 'file_uploads'));
   },
   filename: function (req, file, cb) {
     console.log(file);
     cb(null, Date.now() + path.extname(file.originalname));
   }
-})
+});
 
-var upload = multer({ storage: storage });
+var upload = multer({storage: storage});
 
 
-module.exports = function(app) {
+module.exports = function (app) {
   var env = app.get('env');
 
-  app.set('views', config.root + '/server/views');
+  app.set('views', path.join(config.root, 'serve', 'views'));
   app.set('view engine', 'jade');
   app.use(compression());
-  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.urlencoded({extended: false}));
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
@@ -49,13 +49,14 @@ module.exports = function(app) {
         retrieveGroups: true
       });
 
-      nodeSSPIObj.authenticate(req, res, function(err){
+      nodeSSPIObj.authenticate(req, res, function (err) {
+        console.error(err);
         res.finished || next();
       });
     });
 
     app.use(favicon(path.join(config.root, 'client', 'favicon.ico')));
-    app.use(express.static(path.join(app.get('appPath'), './server/file_uploads/')));
+    app.use(express.static(path.join(app.get('appPath'), '.', 'server', 'file_uploads')));
     app.use(morgan('dev'));
   }
 
